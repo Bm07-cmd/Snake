5969e5f6b5360b2a10dfef08639558bd298e7744
Bm07-cmd <bmai24598@gmail.com>
2025-02-08 10:56:16 -0800

    Update Panel.java

--- Panel.java
+++ Panel.java
@@ -2,11 +2,16 @@ import java.awt.Color;
 import java.awt.Dimension;
 import javax.swing.JPanel;
 import java.awt.Graphics;
+import java.util.ArrayList;
 
 
 public class Panel extends JPanel{
 	Dimension PANEL_SIZE;
 	Rect rect;
+	int TILE_SIZE = 20;
+	int row; 
+	int column;
+	ArrayList<Rect> grid;
 
 	Panel(int w, int h){
 		this.PANEL_SIZE = new Dimension(w, h);
@@ -14,7 +19,19 @@ public class Panel extends JPanel{
 		this.setPreferredSize(PANEL_SIZE);
 		this.setBackground(Color.black);
 
-		this.rect = new Rect(100, 100, 50, 50);
+
+		//this.rect = new Rect(100, 100, 50, 50);
+		this.row = (int)h/TILE_SIZE;
+		this.column = (int)w/TILE_SIZE;
+		this.grid = new ArrayList<>();
+		for(int i = 0; i < row; i++){
+			for(int j = 0; j < column; j++){
+				Rect rect = new Rect(j*TILE_SIZE, i*TILE_SIZE, TILE_SIZE - 2, TILE_SIZE - 2);
+				grid.add(rect);
+			}
+		}
+	
+
 	
 	}
 
@@ -23,7 +40,10 @@ public class Panel extends JPanel{
 		super.paint(g);
 		g.setColor(Color.red);
 		
-		this.rect.draw(g);
+		//this.rect.draw(g);
+		for(int i = 0; i < grid.size(); i++){
+			this.grid.get(i).draw(g);
+		}
 	}
 
 }
