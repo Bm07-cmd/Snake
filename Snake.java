b675b9f90a028716573f206933d7a5d429c5289d
Bm07-cmd <bmai24598@gmail.com>
2025-02-22 10:58:39 -0800

    Update Snake.java

--- Snake.java
+++ Snake.java
@@ -1,19 +1,29 @@
+import java.awt.Graphics;
+import java.awt.Color;
 import java.util.ArrayList;
 
 public class Snake extends Rect {
 	ArrayList<Coordinate> body;
-	int TILE_SIZE = 20;
+	//int TILE_SIZE = 20;
 
 
 	Snake(int x, int y, int w, int h){
 		super(x, y, w, h);
 		this.body = new ArrayList<Coordinate>();
 		Coordinate coordinate = new Coordinate(x, y);
-		Coordinate coordinate2 = new Coordinate(x - TILE_SIZE, y);
-		Coordinate coordinate3 = new Coordinate(x - (TILE_SIZE*2), y);
+		Coordinate coordinate2 = new Coordinate(x - Settings.TILE_SIZE, y);
+		Coordinate coordinate3 = new Coordinate(x - (Settings.TILE_SIZE*2), y);
 		this.body.add(coordinate);
 		this.body.add(coordinate2);
 		this.body.add(coordinate3);
 		
 	}
+
+	public void draw(Graphics g){
+		g.setColor(Color.blue);
+		for(int i = 0; i < this.body.size(); i++){
+			g.fillRect(this.body.get(i).x_pos, this.body.get(i).y_pos, Settings.TILE_SIZE, Settings.TILE_SIZE);
+		}
+	}
+
 }
